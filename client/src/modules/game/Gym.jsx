bd2f53a76ef37a16125a3311b68aef91e08bfbd6
Amir <amir.fatyhow@yandex.ru>
2023-10-23 17:46:37 +0400

    item.jsx -> item.tsx are added

--- client/src/modules/game/Gym.jsx
+++ client/src/modules/game/Gym.jsx
@@ -1,4 +1,4 @@
-import {atom, SetStateAction, useAtom} from "jotai";
+import {atom, useAtom} from "jotai";
 import { useEffect } from "react";
 import { Item } from "./Item";
 import { mapAtom } from "./Lobby";
